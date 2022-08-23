5b729b3da3edcd98cc773c6a5f522a1766c11275
Roberto Huertas <roberto.huertas@outlook.com>
2022-08-23 11:58:24 +0200

    change port

---
+++ README.md
@@ -0,0 +1,5 @@
+# synthentics-tunnel-poc
+
+The app is hosted in Azure => <https://synthetics-poc.azurewebsites.net>
+
+It will be redeployed on every push to master.
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const logger = winston.createLogger({
 });
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
