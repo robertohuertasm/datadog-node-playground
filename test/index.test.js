ad520ae5cc8b07a3fe213e542ee5442a5b81a9cb
Roberto Huertas <roberto.huertas@outlook.com>
2023-06-08 13:27:21 +0200

    ci improvements and flaky tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,13 @@
-test.skip('fails all the time', () => {
+test('fails all the time', () => {
   expect(1).toBe(2);
 });
 
 test('this one works', () => {
   expect(1.5).toBe(1.5);
 });
+
+test('fails in a flaky way', () => {
+  const rnd = Date.now();
+  expect(rnd % 2).toBe(0);
+});
+
