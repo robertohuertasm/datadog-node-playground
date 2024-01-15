6aca7197e7cead917317c428f7391274bf4a0886
Roberto Huertas <roberto.huertas@outlook.com>
2024-01-15 13:51:31 +0100

    logger in its own module

---
+++ logger.js
@@ -0,0 +1,31 @@
+const winston = require('winston');
+const transports = [new winston.transports.Console()];
+
+const apiKey = process.env.APIKEY;
+const commit = process.env.COMMIT;
+const env = process.env.ENV;
+
+if (apiKey) {
+  console.log('Sending logs to Datadog');
+  // https://docs.datadoghq.com/api/latest/logs/
+  let path = `/api/v2/logs?dd-api-key=${apiKey}&ddsource=nodejs&service=datadog-node-playground&ddtags=git.repository_url:github.com/robertohuertasm/datadog-node-playground,env:${env ?? 'development'}`;
+
+  if (commit) {
+    path += `,git.commit.sha:${commit}`;
+  }
+
+  const httpTransportOptions = {
+    host: 'http-intake.logs.datadoghq.com',
+    path,
+    ssl: true
+  };
+  transports.push(new winston.transports.Http(httpTransportOptions))
+}
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports
+});
+
+module.exports = { logger }
